50959ff219e9918f2a5ae2572c960385d6fd7cfb
Marcos <marcaosi2014@gmail.com>
2019-08-03 15:42:48 -0300

    Adiciona entidade Episodios

---
+++ database/migrations/2019_08_03_183519_create_episode.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisode extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('serie_id')->unsigned();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('season');
+            $table->integer('number');
+
+            $table->foreign("serie_id")->references("id")->on("series");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
